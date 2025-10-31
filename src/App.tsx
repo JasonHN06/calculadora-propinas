74c36ad24ab6a0cfc33c1ffee16d99eec813f7cc
Jason Madrid <jasonhnn6@gmail.com>
2025-10-31 13:41:19 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="p-4 m-2 bg-blue-600 text-white font-bold text-center text-4xl">Hola Mundo</h1>
+    </>
+  )
+}
+
+export default App
